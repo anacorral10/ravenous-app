e11a6373b4bf1a98d53088a0944d5659fdafef92
Anastasia Corrales <anacorral10@gmail.com>
2020-05-25 16:48:03 -0400

    Create renderSortByOptions()

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -6,3 +6,14 @@ const sortByOptions = {
   "Highest Rated": "rating",
   "Most Reviewed": "review_count",
 };
+
+class SearchBar extends React.Component {
+  renderSortByOptions() {
+    // Object.keys(sortByOptions) returns an array containing key properties
+    // map(SortByOption returns a callback function that refers to each element in the array
+    return Object.keys(sortByOptions).map((SortByOption) => {
+      let sortByOptionValue = sortByOptions[sortByOption];
+      return <li key={sortByOptionValue}>{sortByOption}</li>;
+    });
+  }
+}
